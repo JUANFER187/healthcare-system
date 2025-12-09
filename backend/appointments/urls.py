f19627fed365a91416c4fb8520ed2ff80a87b707
JUANFER187 <juan.prieto7477@alumnos.udg.mx>
2025-12-08 23:36:00 -0600

    Cambios en appointments/urls

--- backend/appointments/urls.py
+++ backend/appointments/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import ServiceListView, AppointmentListView, AppointmentDetailView
+from .views import (
+    ServiceListView, 
+    AppointmentListView, 
+    AppointmentDetailView,
+    available_slots_view,
+    upcoming_appointments_view
+)
 
 urlpatterns = [
     path('services/', ServiceListView.as_view(), name='services-list'),
+    path('available-slots/', available_slots_view, name='available-slots'),
+    path('upcoming/', upcoming_appointments_view, name='upcoming-appointments'),
     path('', AppointmentListView.as_view(), name='appointments-list'),
     path('<int:pk>/', AppointmentDetailView.as_view(), name='appointment-detail'),
 ]
